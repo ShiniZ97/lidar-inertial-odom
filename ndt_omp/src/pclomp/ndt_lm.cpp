309b30550258a0aaf69ccb7e0f22d2cbceaaa2a6
ShiniZ97 <77226035+ShiniZ97@users.noreply.github.com>
2022-05-16 12:07:30 +0800

    Add files via upload

---
+++ ndt_omp/src/pclomp/ndt_lm.cpp
@@ -0,0 +1,5 @@
+#include <pclomp/ndt_lm.h>
+#include <pclomp/ndt_lm_impl.hpp>
+
+template class omatcher::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ>;
+template class omatcher::NormalDistributionsTransform<pcl::PointXYZI, pcl::PointXYZI>;
